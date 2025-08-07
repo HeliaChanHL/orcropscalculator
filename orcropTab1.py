d86d5dbf2e77f99018343ef2bed58fe871e33bff
Helia Chan <mcswagical@gmail.com>
2025-08-08 00:12:03 +0800

    Update orcropTab1.py

--- orcropTab1.py
+++ orcropTab1.py
@@ -3,4 +3,5 @@ from farmingData import tab1Text
 def modalContent():
 
     st.markdown(tab1Text)
-    st.image("https://media.discordapp.net/attachments/800825909628108800/957490089791995964/png_20220327_150152_0000.png?ex=68961fe2&is=6894ce62&hm=0ab80dcf1de8e655d9a47e487f2d9dcfc5585337c964841724ca68caae2bf7ce&=&format=webp&quality=lossless&width=590&height=1474", caption="OR Crops Prices - by Larsha", use_column_width=True)
+    st.image("https://media.discordapp.net/attachments/800825909628108800/957490089791995964/png_20220327_150152_0000.png?ex=68961fe2&is=6894ce62&hm=0ab80dcf1de8e655d9a47e487f2d9dcfc5585337c964841724ca68caae2bf7ce&=&format=webp&quality=lossless&width=590&height=1474", caption="OR Crops Prices - by Larsha", use_container_width=True)
+
